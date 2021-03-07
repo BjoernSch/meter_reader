60ecfbced20e4b13e0853e538dfa98db03438b41
Björn Schrader <mail@bjoern-schrader.de>
2021-03-07 11:06:43 +0100

    Update meter_reader.py

--- src/meter_reader.py
+++ src/meter_reader.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Reads data via optical interface from eBZ DD3
+# Reads data via optical interface from electronic meters
 # connected to serial Port
 # Publishes Data to InfluxDB
 
@@ -9,84 +9,118 @@ import serial
 import logging
 import threading
 import json
+import requests
 from influxdb import InfluxDBClient
 from datetime import datetime
 
-obis_keys = {
-    b'1-0:0.0.0' : ('owner_id', 'text', False),
-    b'1-0:96.1.0' : ('device_id', 'text', False),
-    b'1-0:1.8.0' : ('consumption_sum_kwh', 'float', True),
-    b'1-0:1.8.1' : ('consumption_t1_kwh', 'float', True),
-    b'1-0:1.8.2' : ('consumption_t2_kwh', 'float', True),
-    b'1-0:16.7.0' : ('power_sum_w', 'float', True),
-    b'1-0:36.7.0' : ('power_l1_w', 'float', True),
-    b'1-0:56.7.0' : ('power_l2_w', 'float', True),
-    b'1-0:76.7.0' : ('power_l3_w', 'float', True),
-    b'1-0:32.7.0' : ('voltage_l1', 'float', True),
-    b'1-0:52.7.0' : ('voltage_l2', 'float', True),
-    b'1-0:72.7.0' : ('voltage_l3', 'float', True),
-    b'1-0:96.5.0' : ('status', 'flags', True),
-    b'0-0:96.8.0' : ('counter', 'hex', False)
-}
 
-class MeterReader(threading.Thread): 
-  def __init__(self, influx, serialport): 
-    threading.Thread.__init__(self)
-    self.ser = serial.Serial(serialport, 9600, parity=serial.PARITY_EVEN, bytesize=7)
-    self.influx = influx
+class MeterReader(threading.Thread):
+    obis_keys = {
+        # eBZ GmbH
+        "ebz" : {
+            b'1-0:0.0.0' : ('owner_id', 'text', False),
+            b'1-0:96.1.0' : ('device_id', 'text', False),
+            b'1-0:1.8.0' : ('consumption_sum_kwh', 'float', True),
+            b'1-0:1.8.1' : ('consumption_t1_kwh', 'float', True),
+            b'1-0:1.8.2' : ('consumption_t2_kwh', 'float', True),
+            b'1-0:16.7.0' : ('power_sum_w', 'float', True),
+            b'1-0:36.7.0' : ('power_l1_w', 'float', True),
+            b'1-0:56.7.0' : ('power_l2_w', 'float', True),
+            b'1-0:76.7.0' : ('power_l3_w', 'float', True),
+            b'1-0:32.7.0' : ('voltage_l1', 'float', True),
+            b'1-0:52.7.0' : ('voltage_l2', 'float', True),
+            b'1-0:72.7.0' : ('voltage_l3', 'float', True),
+            b'1-0:96.5.0' : ('status', 'flags', True),
+            b'0-0:96.8.0' : ('counter', 'hex', False)
+        },
+        # Easymeter
+        "esy" : { 
+            b'1-0:0.0.0' : ('owner_id', 'text', False),
+            b'1-0:96.1.255' : ('device_id', 'text', False),
+            b'1-0:1.8.0' : ('consumption_sum_kwh', 'float', True),
+            b'1-0:2.8.0' : ('delivery_sum_kwh', 'float', True),
+            b'1-0:15.8.0' : ('total_sum_kwh', 'float', True),
+            b'1-0:1.7.0' : ('power_sum_w', 'float', True),
+            b'1-0:21.7.0' : ('power_l1_w', 'float', True),
+            b'1-0:41.7.0' : ('power_l2_w', 'float', True),
+            b'1-0:61.7.0' : ('power_l3_w', 'float', True),
+            b'1-0:96.5.5' : ('status', 'flags', True)
+        }
+    }
 
-  def run(self):
-    start = False
-    while True:
-        line = self.ser.readline().strip()
-        logging.debug(f'Line: {line}')
-        if len(line) == 0:
-            pass
-        elif line[0] == ord('/'):
-            logging.debug('Start of frame')
-            start = True
-            timestamp = datetime.now()
-            data = dict()
-            lineparts = line[1:].split(b'_')
-            data['model_id'] = (str(lineparts[0], 'utf-8'), False)
-            data['version'] = (str(lineparts[1], 'utf-8'), False)
-        elif line[0] == ord('!'):
-            logging.debug('End of frame')
-            if start == True:
-                self.influx.write_points([
-                    {
-                        {
-                            "measurement": data['device_id'][0]
-                        },
+    def __init__(self, influx, serialport): 
+        threading.Thread.__init__(self)
+        
+        try:
+            self.ser = serial.Serial(serialport, 9600, parity=serial.PARITY_EVEN, bytesize=7)
+        except SerialException:
+            logging.error('Error opening port "{serialport}"!')
+            exit(2)
+            
+        self.influx = influx
+
+    def run(self):
+        start = False
+        points = []
+        while True:
+            line = self.ser.readline().strip()
+            logging.debug(f'Line: {line}')
+            if len(line) == 0:
+                pass
+            elif line[0] == ord('/'):
+                logging.debug('Start of frame')
+                timestamp = datetime.now()
+                data = dict()
+                
+                lineparts = line[1:].split(b'_')
+                data['model_id'] = (str(lineparts[0], 'utf-8'), False)
+                data['version'] = (str(lineparts[1], 'utf-8'), False)
+                
+                model = str(data['model_id'][0][0:3]).lower()
+                
+                if model in self.obis_keys:
+                    start = True
+                    logging.debug('Model {model}')
+                else:
+                    logging.error('Unknown model "{model}"!')
+            elif line[0] == ord('!'):
+                logging.debug('End of frame')
+                if start == True:
+                    points += [{
+                        "measurement": data['device_id'][0],
                         "fields": {k: v for k, (v, log) in data.items() if log == True},
                         "time": timestamp.isoformat()
-                    }
-                ])
-                start = False
-        elif start == True:
-            lineparts = line.split(b"(")
-            key = lineparts[0].split(b"*")[0]
-            value = lineparts[1].rstrip(b")").split(b"*")[0]
-            if key in obis_keys:
-                value_name, value_type, log_value = obis_keys[key]
-                if value_type == 'float':
+                    }]
                     try:
-                        data[value_name] = (float(value), log_value)
-                    except ValueError:
-                        logging.error(f'ValueError in {value_name}: {value} is not a float.')
-                elif value_type == 'text':
-                    data[value_name] = (str(value, 'utf-8').strip(), log_value)
-                elif value_type == 'flags':
-                    # Nothing of interest known in the flags, just save the int
-                    try:
-                        data[value_name] = (int(value, 16), log_value)
-                    except ValueError:
-                        logging.error(f'ValueError in {value_name}: {value} is not a base 16 int.')
-                elif value_type == 'hex':
-                    try:
-                        data[value_name] = (int(value, 16), log_value)
-                    except ValueError:
-                        logging.error(f'ValueError in {value_name}: {value} is not a base 16 int.')
+                        self.influx.write_points(points)
+                        points = []
+                    except requests.exceptions.ConnectionError:
+                        logging.error('Error sending data points! {} points cached.'.format(len(points)))
+                    start = False
+            elif start == True:
+                lineparts = line.split(b"(")
+                key = lineparts[0].split(b"*")[0]
+                value = lineparts[1].rstrip(b")").split(b"*")[0]
+                if key in self.obis_keys[model]:
+                    value_name, value_type, log_value = self.obis_keys[model][key]
+                    if value_type == 'float':
+                        try:
+                            data[value_name] = (float(value), log_value)
+                        except ValueError:
+                            logging.error(f'ValueError in {value_name}: {value} is not a float.')
+                    elif value_type == 'text':
+                        data[value_name] = (str(value, 'utf-8').strip(), log_value)
+                    elif value_type == 'flags':
+                        # Nothing of interest known in the flags, just save the int
+                        try:
+                            data[value_name] = (int(value, 16), log_value)
+                        except ValueError:
+                            logging.error(f'ValueError in {value_name}: {value} is not a base 16 int.')
+                    elif value_type == 'hex':
+                        try:
+                            data[value_name] = (int(value, 16), log_value)
+                        except ValueError:
+                            logging.error(f'ValueError in {value_name}: {value} is not a base 16 int.')
 
 if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
