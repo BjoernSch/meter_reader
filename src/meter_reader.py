fe46cb059ae3fe4c54f23ffe19ec7d4eee14b598
BjoernSch <BjoernSch@users.noreply.github.com>
2024-05-22 10:20:43 +0200

    Add MQTT publishing

--- src/meter_reader.py
+++ src/meter_reader.py
@@ -13,7 +13,7 @@ import requests
 from glob import glob
 from influxdb import InfluxDBClient
 from datetime import datetime
-
+import paho.mqtt.client as mqtt_client
 
 class MeterReader(threading.Thread):
     obis_keys = {
@@ -53,12 +53,12 @@ class MeterReader(threading.Thread):
         }
     }
 
-    def __init__(self, influx, serialport): 
+    def __init__(self, mqttc, influx, serialport): 
         threading.Thread.__init__(self)
         
         try:
             self.ser = serial.Serial(serialport, 9600, parity=serial.PARITY_EVEN, bytesize=7)
-        except SerialException:
+        except serial.SerialException:
             logging.error('Error opening port "{serialport}"!')
             exit(2)
             
@@ -91,16 +91,22 @@ class MeterReader(threading.Thread):
             elif line[0] == ord('!'):
                 logging.debug('End of frame')
                 if start == True:
-                    points += [{
-                        "measurement": data['device_id'][0],
-                        "fields": {k: v for k, (v, log) in data.items() if log == True},
-                        "time": timestamp.isoformat()
-                    }]
-                    try:
-                        self.influx.write_points(points)
-                        points = []
-                    except requests.exceptions.ConnectionError:
-                        logging.error('Error sending data points! {} points cached.'.format(len(points)))
+                    if self.influx:
+                        points += [{
+                            "measurement": data['device_id'][0],
+                            "fields": {k: v for k, (v, log) in data.items() if log == True},
+                            "time": timestamp.isoformat()
+                        }]
+                        try:
+                            self.influx.write_points(points)
+                            points = []
+                        except requests.exceptions.ConnectionError:
+                            logging.error('Error sending data points! {} points cached.'.format(len(points)))
+                    if self.mqttc:
+                        for key, (value, log) in data.items():
+                            if log == True:
+                                self.mqttc.publish(f"/{mqtt_topic}/{data['device_id'][0]}/{key}", value)
+                    
                     start = False
             elif start == True:
                 lineparts = line.split(b"(")
@@ -127,19 +133,46 @@ class MeterReader(threading.Thread):
                         except ValueError:
                             logging.error(f'ValueError in {value_name}: {value} is not a base 16 int.')
 
+def mqtt_on_connect(client, userdata, flags, reason_code, properties):
+    if reason_code.is_failure:
+        logging.error(f"Failed to connect: {reason_code}. loop_forever() will retry connection")
+    else:
+        # we should always subscribe from on_connect callback to be sure
+        # our subscribed is persisted across reconnections.
+        None
+
 if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO,
                         datefmt="%H:%M:%S")
+    
+    if os.getenv('MQTT_HOST') != None:
+        mqtt_host = os.getenv('MQTT_HOST')
+        mqtt_port = int(os.getenv('MQTT_PORT', '1883')) 
+        mqtt_username = os.getenv('MQTT_USER')
+        mqtt_password = os.getenv('MQTT_PASSWORD')
+        mqtt_topic = os.getenv('MQTT_TOPIC', 'meter')
 
-    influx_host = os.getenv('INFLUXDB_HOST', 'influxdb.influxdb')
-    influx_port = int(os.getenv('INFLUXDB_PORT', '8086')) 
-    influx_username = os.getenv('INFLUXDB_USER', 'meter_reader')
-    influx_password = os.getenv('INFLUXDB_PASSWORD')
-    influx_database = os.getenv('INFLUXDB_DATABASE', 'meter_reader')
+        mqttc = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION2)
+        mqttc.on_connect = mqtt_on_connect
+        mqttc.enable_logger()
+        mqttc.loop_start()
+        mqttc.username_pw_set(mqtt_username, mqtt_password)
+        mqttc.connect(mqtt_host, mqtt_port, 60)
+    else:
+        mqttc = None
+    
+    if os.getenv('INFLUXDB_HOST') != None:
+        influx_host = os.getenv('INFLUXDB_HOST')
+        influx_port = int(os.getenv('INFLUXDB_PORT', '8086')) 
+        influx_username = os.getenv('INFLUXDB_USER', 'meter_reader')
+        influx_password = os.getenv('INFLUXDB_PASSWORD')
+        influx_database = os.getenv('INFLUXDB_DATABASE', 'meter_reader')
 
-    influx = InfluxDBClient(influx_host, influx_port, influx_username, influx_password, influx_database)
-
+        influx = InfluxDBClient(influx_host, influx_port, influx_username, influx_password, influx_database)
+    else:
+        influx = None
+    
     threads = list()
     serial_ports = glob('/dev/ttyMETER*')
     if len(serial_ports) == 0:
@@ -147,7 +180,7 @@ if __name__ == "__main__":
     else:
         for serialport in serial_ports:
             logging.info(f"Create and start thread for {serialport}")
-            x = MeterReader(influx, serialport)
+            x = MeterReader(mqttc, influx, serialport)
             threads.append(x)
             x.start()
 
